2241f6ad852f58eaf2fe0ca705695e31b45946b7
longsun <longsun@qunhemail.com>
2020-12-17 13:12:55 +0800

    main

--- gaoxi-user/src/main/java/com/gaoxi/user/GaoxiUserApplication.java
+++ gaoxi-user/src/main/java/com/gaoxi/user/GaoxiUserApplication.java
@@ -2,9 +2,19 @@ package com.gaoxi.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author anyun
+ */
 @SpringBootApplication
-public class GaoxiUserApplication {
+public class GaoxiUserApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GaoxiUserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaoxiUserApplication.class, args);
